b715af618c8fa7c595d580a8a6d0e051257b96c0
kangz <kangz@WHPC130218.oplinkwh.com.cn>
2013-12-05 10:40:24 +0800

    aa

---
+++ ygplatform/src/main/java/hello/GreetingController.java
@@ -0,0 +1,17 @@
+package hello;
+
+/**
+ *
+ * <p>Title: </p>
+ * <p>Description:  </p>
+ * <p>Copyright: Copyright (c) 2013 </p>
+ * <p>Company: Oplink Communications</p>
+ * @author 
+ ** @serialData 2013-11-29 下午4:33:26
+ * @version 1.0
+ */
+
+public class GreetingController {
+
+}
+
